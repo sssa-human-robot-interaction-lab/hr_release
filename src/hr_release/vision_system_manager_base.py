42b911461b5d8c1fbff4f1718f4ca3c2c7bfb228
penzottimattia <penzottimattia@gmail.com>
2022-08-13 13:19:06 +0200

    fixed kinematics diff

--- src/hr_release/vision_system_manager_base.py
+++ src/hr_release/vision_system_manager_base.py
@@ -1,7 +1,7 @@
 import numpy as np
 from threading import Lock, Thread
 from functools import partial
-from queue import Queue
+from collections import deque
 
 import rospy, tf, tf.transformations as ts
 
@@ -11,14 +11,19 @@ from cartesian_control_msgs.msg import CartesianTrajectoryPoint
 from artificial_hands_msgs.msg import CartesianTrajectoryPointStamped
 from artificial_hands_py.artificial_hands_py_base import *
 
-def sav_gol(q : Queue):
+def sav_gol(queue : deque, n_sav_gol : int):
   s = 0
-  g = [0.08333,0.05952,0.03571,0.0119,-0.0019,-0.03571,-0.05952,-0.08333]
-  for c in g:
-    s += c*q.get()
+  if n_sav_gol == 4:
+    g = [0.3,0.1,-0.1,-0.3]
+  elif n_sav_gol == 8:
+    g = [0.08333,0.05952,0.03571,0.0119,-0.0019,-0.03571,-0.05952,-0.08333]
+  for c, q in zip(g, queue):
+    s += c*q
   return s
 
 class VisionPoint:
+
+  lock = Lock()
 
   def __init__(self, frame_id : str, subject : str, callback, broadcaster) -> None:
     
@@ -31,10 +36,10 @@ class VisionPoint:
     self.xdd = []
     
     self.n_sav_gol = 8
-    for n in range(self.n_sav_gol):
-      self.x.append(Queue(maxsize = self.n_sav_gol))
-      self.xd.append(Queue(maxsize = self.n_sav_gol))
-      self.xdd.append(Queue(maxsize = self.n_sav_gol))
+    for n in range(7):
+      self.x.append(deque(maxlen = self.n_sav_gol))
+      self.xd.append(deque(maxlen = self.n_sav_gol))
+      self.xdd.append(deque(maxlen = self.n_sav_gol))
 
     sub = rospy.Subscriber('vicon/'+subject,TransformStamped,partial(callback,self.pnt))
     self.pub = rospy.Publisher('vicon_state/'+subject,CartesianTrajectoryPointStamped,queue_size=1000)
@@ -42,36 +47,41 @@ class VisionPoint:
     self.br : tf. TransformBroadcaster = broadcaster
 
     d_thread = Thread(target=self.update_d)
-    #d_thread.start
+    d_thread.start()
   
   def update_d(self):
 
-    rate = rospy.Rate(30)
+    rate_hz = 50
+
+    rate = rospy.Rate(rate_hz)
 
     while not rospy.is_shutdown():
 
+      self.lock.acquire()
+
       for (q, p) in (zip(self.x,pose_to_list(self.pnt.point.pose))):
-        q.put(p)
+        q.append(p)
 
-      if self.x[0].full():
-        td = []
-        for (qd, q) in (zip(self.xd,self.x)):
-          sd = sav_gol(q)
-          qd.put(sd)
-          td.append(sd)
-
-      if self.xd[0].full():
-        tdd = []
-        for (qdd, qd) in (zip(self.xdd,self.xd)):
-          sdd = sav_gol(qd)
-          qdd.put(sdd)
-          tdd.append(sdd)
-
+      td = []
+      for (qd, q) in (zip(self.xd,self.x)):
+        sd = rate_hz*sav_gol(q,self.n_sav_gol)
+        qd.append(sd)
+        td.append(sd)
       self.pnt.point.twist = list_to_twist(td)
+
+      tdd = []
+      for (qdd, qd) in (zip(self.xdd,self.xd)):
+        sdd = rate_hz*sav_gol(qd,self.n_sav_gol)
+        qdd.append(sdd)
+        tdd.append(sdd)
       self.pnt.point.acceleration = list_to_twist(tdd)
+      
+      self.lock.release()
+
       rate.sleep() 
 
   def update(self):
+    self.pnt.header.stamp = rospy.Time.now()
     self.pub.publish(self.pnt)
     self.br.sendTransform([self.pnt.point.pose.position.x,self.pnt.point.pose.position.y,self.pnt.point.pose.position.z],
       quat_to_list(self.pnt.point.pose.orientation),rospy.Time.now(),self.pnt.header.frame_id,"base")
@@ -103,7 +113,7 @@ class VisionSystem:
 
     self.pnts = [self.rec_pnt,self.giv_pnt,self.obj_pnt]
 
-    tf_tim = rospy.Timer(rospy.Duration(0.01), self.update_tf)
+    tf_tim = rospy.Timer(rospy.Duration(0.025), self.update_tf)
 
   def reset_calibration_matrix(self):
     self.R = ts.rotation_matrix(0,[0,0,1])
@@ -142,6 +152,18 @@ class VisionSystem:
    
   def vision_cb(self, pnt : CartesianTrajectoryPointStamped, msg : TransformStamped): 
     self.lock.acquire()
-    pnt.header.stamp = msg.header.stamp
     pnt.point.pose : Pose = matrix_to_pose(np.dot(self.R,transform_to_matrix(msg.transform)))
     self.lock.release()
+
+def main():
+
+  rospy.init_node('vision_system_manager_node')
+
+  vis = VisionSystem()
+
+  rospy.loginfo('Vision system manager ready!')
+
+  rospy.spin()
+
+if __name__ == '__main__':
+  main()
